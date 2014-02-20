c3ad7961c1fbd164a0f6d6266bb19228d52bde9f
Steve Morin <steve.morin@gmail.com>
2014-02-20 14:37:12 -0800

    updating the ping tests, checksum of ISO, bash bug fix and updated mirror reference

--- vars.sh
+++ vars.sh
@@ -6,10 +6,10 @@ GUESTADDITIONS_PATH=""
 OS_NAME=`uname`
 
 #Determining OS and setting VBoxGuestAdditions default path accordingly
-if [ $OS_NAME=="Linux" ]; then
+if [ $OS_NAME == "Linux" ]; then
     # Default Linux Location of Guest Additions
 	GUESTADDITIONS_PATH=/usr/share/virtualbox/VBoxGuestAdditions.iso
-elif [ $OS_NAME=="Darwin"]; then
+elif [ $OS_NAME == "Darwin" ]; then
 	# Default MAC Location of Guest Additions
 	GUESTADDITIONS_PATH=/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso
 else
@@ -22,5 +22,6 @@ HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
 HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
 NATNET=10.0.2.0/24
 
-CENTOS_6_4_MINIMAL_URL=http://vault.centos.org/6.4/isos/x86_64/CentOS-6.4-x86_64-minimal.iso
+# Main vault location to find a mirror: http://vault.centos.org/6.4/isos/x86_64/CentOS-6.4-x86_64-minimal.iso
+CENTOS_6_4_MINIMAL_URL=http://mirror.symnds.com/distributions/CentOS-vault/6.4/isos/x86_64/CentOS-6.4-x86_64-minimal.iso
 CENTOS_6_5_MINIMAL_URL=http://mirror.rackspace.com/CentOS/6.5/isos/x86_64/CentOS-6.5-x86_64-minimal.iso
