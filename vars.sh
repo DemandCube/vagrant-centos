51b2a6a26c6e689d3031652986c3caad36a368ff
Hikmat Dhamee <me.hemant.available@gmail.com>
2014-02-20 08:43:43 +0545

    add error handling and typo code

--- vars.sh
+++ vars.sh
@@ -13,13 +13,14 @@ elif [ $OS_NAME=="Darwin"]; then
 	# Default MAC Location of Guest Additions
 	GUESTADDITIONS_PATH=/Applications/VirtualBox.app/Contents/MacOS/VBoxGuestAdditions.iso
 else
-	echo "Couldn't determine VBoxGuestAdditions path!!!"
+	echo "ERROR: Couldn't determine VBoxGuestAdditions path!!!.Find manually and update vars.sh with the right path. Aborting..."
+	exit 1
 fi
 
 GUESTADDITIONS="./isos/VBoxGuestAdditions-4.3.6.iso"
 HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
 HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
-NATNET=192.168.1.0/24
+NATNET=10.0.2.0/24
 
 CENTOS_6_4_MINIMAL_URL=http://vault.centos.org/6.4/isos/x86_64/CentOS-6.4-x86_64-minimal.iso
 CENTOS_6_5_MINIMAL_URL=http://mirror.rackspace.com/CentOS/6.5/isos/x86_64/CentOS-6.5-x86_64-minimal.iso
